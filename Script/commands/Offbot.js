3c346017d5ebf4128a7d5aed5782ffbe6e1e6878
Jehad2441139 <joyjehad58@gmail.com>
2025-07-27 07:20:10 +0600

    Update Offbot.js

--- Script/commands/Offbot.js
+++ Script/commands/Offbot.js
@@ -9,6 +9,6 @@ module.exports.config = {
         };
 module.exports.run = ({event, api}) =>{
     const permission = ["100082395531611", "100086680386976"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For ULLASH", event.threadID, event.messageID);
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For JEHAD JOY", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
